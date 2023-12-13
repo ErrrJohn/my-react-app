a89c8e94c8256325cbc5565bf14141c90b48fcf3
Vin <36500467+ErrrJohn@users.noreply.github.com>
2023-12-13 17:36:21 +0800

    skills

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ import Info from './Info';
 
 const About = () => {
   return (
-    <section className="about section #about">
+    <section id="about" className="about section">
         <h2 className="section__title">About Me</h2>
         <span className="section__subtitle">My Introduction</span>
 
